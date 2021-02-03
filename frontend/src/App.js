5c9fe2e7aad9aec59c3639c799f62bde98fa4e41
coderjaisharma <jaisharma.info@gmail.com>
2021-02-03 22:33:37 +0530

    item

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,56 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Header from './component/Header'
+import Item from './component/Item.js'
+import ModelS from '../src/assets/Desktop-ModelS.jpeg'
+import ModelX from '../src/assets/Desktop-ModelX.jpeg'
+import ModelY from '../src/assets/Desktop-ModelY.jpeg'
+import SolarPanel from '../src/assets/Desktop-SolarPanels.jpeg'
+import SolarRoof from '../src/assets/Desktop-SolarRoof.jpeg'
 function App() {
   return (
     <div className="App">
-   <div className="home__page">
-   <div className="home__header">
-<Header/>
+
+    <Header/>
+   <div className="app__itemscontainer">
+   
+   <div className="home__cont">
+   <Item title="Model X" imgsrc={ModelX} desc=" Order Online for Touchless Delivery"
+      descLink=""
+    leftBtnTxt="ORDER NOW" 
+    leftBtnLink="" 
+    rightBtnTxt="LEARN MORE"
+    rightBtnLink="" 
+    twoButtons='true' 
+    first
+   />
+   <Item title="Model Y"imgsrc={ModelY} 
+    descLink=""
+    leftBtnTxt="ORDER NOW" 
+    leftBtnLink="" 
+    rightBtnTxt="LEARN MORE"
+    rightBtnLink="" 
+    twoButtons='true' 
+    
+    />
+   <Item title="Model S"imgsrc={ModelS}
+     descLink=""
+    leftBtnTxt="ORDER NOW" 
+    leftBtnLink="" 
+    rightBtnTxt="LEARN MORE"
+    rightBtnLink="" 
+    twoButtons='true'
+   />
+   <Item title="SOLAR PANEL"imgsrc={SolarPanel}
+     descLink=""
+    leftBtnTxt="ORDER NOW" 
+    leftBtnLink="" 
+    rightBtnTxt="LEARN MORE"
+    rightBtnLink="" 
+    twoButtons='true'
+   />
+
    </div>
-   <div className="home__cont"></div>
    <div className="home__footer"></div>
 
    </div>
---
+++ frontend/src/component/Item.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Item.css'
+import Button from './Button'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+const Item = (props) => {
+    return (
+        <div className="item" style={{backgroundImage:`url(${props.imgsrc})`}} >
+            <div className="item__container" >
+            
+                <div className="item__text">
+                    <p>{props.title}</p>
+                    </div>
+                    <div className="item__textDesc">
+                        <p>{props.desc}</p>
+                   
+                </div>
+                <div className="item__lowerThird">
+                    <div className="item__buttons">
+                          <Button imp="primary" text={props.leftBtnLink} link={props.leftBtnLink} />
+                          {props.twoButtons && (<Button imp="secondary" text={props.rightBtnTxt} link={props.rightBtnLink}/>)}
+                    </div>
+                    {props.first &&(<div className="item__expand">
+                    <ExpandMoreIcon/>
+                    </div>)}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Item
